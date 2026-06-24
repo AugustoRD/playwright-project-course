424edc2ff5dd56fef969527688eee0a4ebb5a6b9
AugustoRD <augusto06r@gmail.com>
2026-06-24 10:35:29 -0300

    feat: enhance LoginPage with error message handling and refactor login tests to use data-driven approach

--- tests/login_test.spec.ts
+++ tests/login_test.spec.ts
@@ -1,14 +1,31 @@
-import {test} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 import {LoginPage} from '../pages/LoginPage';
+import loginData from '../test-data/loginData.json';
 
-test('Login Test', async ({page}) => {
+test('Valid Login Test', async ({page}) => {
 
     const loginPage = new LoginPage(page);
 
     await loginPage.gotoLoginPage();
 
-    await loginPage.login('standard_user', 'secret_sauce');
+    //await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login(loginData.validUser.username, loginData.validUser.password);
 
-    await loginPage.expectLoginSuccess();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+
+});
+
+test('Invalid Login Test', async ({page}) => {
+
+    const loginPage = new LoginPage(page);
+
+    await loginPage.gotoLoginPage();
+
+    //await loginPage.login('standard_user', 'secret_sauce');
+    await loginPage.login(loginData.invalidUser.username, loginData.invalidUser.password);
+
+    await expect(loginPage.errorMessage).toBeVisible();
+
 
 });
